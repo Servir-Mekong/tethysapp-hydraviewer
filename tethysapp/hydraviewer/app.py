10f3d080de344cad12c6c1e92b2f3533785f7771
kmarkert <kel.markert@gmail.com>
2018-07-11 12:54:06 -0500

    initial commit

---
+++ tethysapp/hydraviewer/app.py
@@ -0,0 +1,34 @@
+from tethys_sdk.base import TethysAppBase, url_map_maker
+
+
+class Hydraviewer(TethysAppBase):
+    """
+    Tethys app class for HYDRA Viewer.
+    """
+
+    name = 'HYDRA Viewer'
+    index = 'hydraviewer:home'
+    icon = 'hydraviewer/images/icon.gif'
+    package = 'hydraviewer'
+    root_url = 'hydraviewer'
+    color = '#f39c12'
+    description = 'Place a brief description of your app here.'
+    tags = '&quot;Remote-Sensing&quot;,&quot;Floods&quot;'
+    enable_feedback = False
+    feedback_emails = []
+
+    def url_maps(self):
+        """
+        Add controllers
+        """
+        UrlMap = url_map_maker(self.root_url)
+
+        url_maps = (
+            UrlMap(
+                name='home',
+                url='hydraviewer',
+                controller='hydraviewer.controllers.home'
+            ),
+        )
+
+        return url_maps
