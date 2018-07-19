01cdec699b6d652851f174df90db959e5b33826b
kmarkert <kel.markert@gmail.com>
2018-07-19 16:03:54 -0500

    adding ajax controllers for historical water maps

---
+++ tethysapp/hydraviewer/ajax_controllers.py
@@ -0,0 +1,31 @@
+import ee
+from hydra import geeutils
+import json
+from django.http import JsonResponse
+import datetime
+
+ee.Initialize()
+
+region = ee.Geometry.Rectangle([-180,-90,180,90])
+
+def update_historical(request):
+    return_obj = {}
+
+    if request.method == 'POST':
+        try:
+            info = request.POST;
+            start = info.get('sDate')
+            end = info.get('eDate')
+            month = info.get('month')
+            algo = info.get('algo')
+            climo = bool(int(info.get('climo')))
+
+            water_layer = geeutils.historicalMap(region,start,end,month=month,algorithm=algo,climatology=climo)
+
+            return_obj["url"] = water_layer
+            return_obj["success"] = "success"
+
+        except Exception as e:
+            return_obj["error"] = "Error Processing Request. Error: "+ str(e)
+
+    return JsonResponse(return_obj)
