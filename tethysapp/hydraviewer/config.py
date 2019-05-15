52f0cbb3850376d0481eb8896763c157a9395bee
kmarkert <kel.markert@gmail.com>
2019-05-15 01:53:51 -0500

    changed configuration file for Myanmar delivery

--- tethysapp/hydraviewer/config.py
+++ tethysapp/hydraviewer/config.py
@@ -1,5 +1,5 @@
 # Replace this with the processing bounding box for your region
-WEST, SOUTH, EAST, NORTH = 92.0, 5.5, 109.5, 28.5
+WEST, SOUTH, EAST, NORTH = 92.0, 9.5, 101.5, 29
 BOUNDING_BOX = (WEST,SOUTH,EAST,NORTH)
 
 # Replace this with the service account and secret key file
@@ -7,4 +7,4 @@ SERVICEACCOUNT = ''
 KEYFILE = ''
 
 # Replace this with the asset collection name with the surface water data
-WATERCOLLECTION = 'projects/servir-mekong/hydrafloods/surface_water'
+WATERCOLLECTION = 'projects/servir-mekong/hydrafloods/use_cases/hydra_extents'
